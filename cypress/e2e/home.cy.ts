ec62e66b9ff1a040a21069014fe9908376530cc6
Manzell Hatten <manzellhatten@gmail.com>
2025-03-04 12:20:50 -0500

    First Lesson of Cypress Completed

---
+++ cypress/e2e/home.cy.ts
@@ -0,0 +1,13 @@
+describe('home page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+
+  it('the h1 contains the correct text', () => {
+    cy.get("[data-test='hero-heading']").contains("Testing Next.js Applications with Cypress")
+  })
+
+  it.only('the features on the homepage are correct', () => {
+    cy.get('dt').eq(0).contains("4 Courses")
+  })
+})
